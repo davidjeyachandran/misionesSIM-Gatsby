10191135081eb93afc88daa872235f8140df805c
David Jeyachandran <david.jeyachandran@sonder.io>
2023-07-05 17:40:37 +0200

    Add test img to homepage

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -9,6 +9,7 @@ import Layout from '../components/Layout';
 // types
 import type { SingleRevista } from '../types/types';
 import RevistaList from '../components/RevistaList';
+import { StaticImage } from 'gatsby-plugin-image';
 
 type GraphQLResult = {
   allContentfulRevista: {
@@ -18,6 +19,15 @@ type GraphQLResult = {
 
 const Home = ({ location }: PageProps<GraphQLResult>) => (
   <Layout location={location}>
+       <Container maxWidth={false}>
+        People
+        <StaticImage 
+          alt='Header image of group of people'
+          src='../assets/slider1.jpg'
+          placeholder="blurred"
+          layout='fullWidth'
+        />
+    </Container>
     <Container maxWidth='lg'>
       <h1>Home</h1>
     </Container>
