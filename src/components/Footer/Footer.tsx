c02f5362419efe8d0c39ea7141583e62a0ef3820
David Jeyachandran <davidjeyachandran@gmail.com>
2023-07-30 21:53:09 +1000

    Add Fontawesome

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -1,31 +1,45 @@
 import React from 'react';
 import logo from './logo-reversed.png'
-
-// components
 import { Container, Grid, Typography } from '@mui/material';
-
-// styled components
-import * as S from './styles';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import {
+	faFacebook,
+	faInstagram,
+	faYoutube,
+	faLinkedin,
+	faWhatsapp,
+} from '@fortawesome/free-brands-svg-icons'
 
 const Footer = () => (
-	<Container maxWidth={false}>
-		<S.Footer>
-			<Grid container spacing={2}>
-				<Grid item xs={12} sm={6} sx={{ mx: 4 }}>
-					<img src={logo} />
-					<Typography color="grey" component="p" sx={{ fontSize: '1.1em' }}>
-						La visión de SIM es un testimonio de comunidades de creyentes en Jesús entre los pueblos menos alcanzados del mundo.
-						Cruzamos barreras para proclamar al Cristo crucificado y resucitado, expresando Su amor y compasión entre aquellos que viven y mueren sin Él.<br />
-						Hacemos discípulos que confían y obedecen a Jesús, y llegan a integrarse a iglesias Cristo céntricas.<br />
-						Trabajamos en conjunto con las iglesias para cumplir la misión de Dios entre culturas tanto local como globalmente.<br />
-						Facilitamos la participación en ministerios transculturales de aquellos a quien Dios está llamando.
-					</Typography>
-				</Grid>
-				<Grid item xs={12} sm={6}>
+	<Container maxWidth={false} sx={{ backgroundColor: 'black' }}>
+		<Grid container >
+			<Grid item xs={12} sm={6} height='100' sx={{ backgroundColor: 'yellow' }}>
 
-				</Grid>
+				Lorem ipsum dolor sit amet consectetur, adipisicing elit. Accusamus, dolorum! Fugiat dicta magnam tempore est eos quo quaerat enim eaque ex porro culpa, cumque sint, obcaecati suscipit earum nulla. Saepe?
 			</Grid>
-		</S.Footer>
+			<Grid item xs={12} sm={6} height='100' sx={{ backgroundColor: 'lime' }}>
+				Lorem, ipsum dolor sit amet consectetur adipisicing elit. Ducimus sit, inventore sunt a ullam provident quod adipisci quibusdam? Doloremque, repellat soluta odit fugiat temporibus voluptate explicabo? Voluptatum iusto aspernatur cumque.
+			</Grid>
+		</Grid>
+		<Grid container sx={{ color: 'white', fontSize: '1em' }}>
+			<Grid item xs={12} sm={6} >
+				<img src={logo} />
+				<p>Cruzamos barreras para proclamar al Cristo crucificado y resucitado, expresando Su amor y compasión entre aquellos que viven y mueren sin Él.</p>
+				<p>Hacemos discípulos que confían y obedecen a Jesús, y llegan a integrarse a iglesias Cristo céntricas.</p>
+				<p>Trabajamos en conjunto con las iglesias para cumplir la misión de Dios entre culturas tanto local como globalmente.</p>
+				<p>Facilitamos la participación en ministerios transculturales de aquellos a quien Dios está llamando.</p>
+			</Grid>
+			<Grid item xs={12} sm={6}>
+				<FontAwesomeIcon icon={faFacebook} size="2x" />
+				<FontAwesomeIcon icon={faInstagram} size="2x" />
+				<FontAwesomeIcon icon={faYoutube} size="2x" />
+				<FontAwesomeIcon icon={faLinkedin} size="2x" />
+				<FontAwesomeIcon icon={faWhatsapp} size="2x" />
+
+				¿Tienes alguna pregunta? Escríbenos a sim.preguntas@gmail.com
+
+			</Grid>
+		</Grid>
 	</Container>
 );
 
